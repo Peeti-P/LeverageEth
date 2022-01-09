809e86d42b902168ff661540db7cbd99584df9cb
Peeti-P <86291241+Peeti-P@users.noreply.github.com>
2022-01-09 14:20:22 +0800

    Update Lev2.sol

--- Lev2.sol
+++ Lev2.sol
@@ -37,93 +37,161 @@ interface CEth {
     function exchangeRateStored() external view returns (uint);
 }
 
-//0x9A536Ed5C97686988F93C9f7C2A390bF3B59c0ec price oracle contract
-interface PriceFeed {
-    function getUnderlyingPrice(address cToken) external view returns (uint);
+interface uniswapRounter {
+    function WETH() external pure returns (address);
+    function swapExactTokensForETH(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline) external returns (uint[] memory amounts);
+    function swapExactETHForTokens(uint amountOutMin, address[] calldata path, address to, uint deadline) external payable returns (uint[] memory amounts);
+}
+
+//0x7BBF806F69ea21Ea9B8Af061AD0C1c41913063A1 price oracle contract
+interface EthPriceFeed {
+    function getUnderlyingPrice(CEth) external view returns (uint);
 }
 
 
 contract LeveragePls {
-    // event MyLog(string, uint256);
-    // address payable CEth_address = payable(0x20572e4c090f15667cF7378e16FaD2eA0e2f3EfF);
+    // Address of the deployed contracts on Ropsten Testnet
+    address uniswap_rounter_address = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
     address cDai_address = 0xbc689667C13FB2a04f09272753760E38a95B998C;
     address cEth_adress = 0x859e9d8a4edadfEDb5A2fF311243af80F85A91b8;
+    address dai_address = 0x31F42841c2db5173425b5223809CF3A38FEde360;
+    uniswapRounter uniswap_interface = uniswapRounter(uniswap_rounter_address);
     Comptroller comp_interface = Comptroller(0xcfa7b0e37f5AC60f3ae25226F5e39ec59AD26152);
-    Erc20 Dai_interface = Erc20(0x31F42841c2db5173425b5223809CF3A38FEde360);
+    EthPriceFeed oracle_interface = EthPriceFeed(0x7BBF806F69ea21Ea9B8Af061AD0C1c41913063A1);
+    Erc20 Dai_interface = Erc20(dai_address);
     CEth Ceth_interface = CEth(cEth_adress);
     CErc20 cDai_interface = CErc20(cDai_address);
 
-
-    function supplyEthToCompound()
-        public
-        payable
-        returns (bool)
-    {
-        // Mint CEth
-        Ceth_interface.mint{ value: msg.value, gas: 300000 }();
+    function openPosition() 
+        public 
+        payable 
+        returns (bool) {
+        uint contributeAmount = msg.value;
+        uint EthPrice = EthpriceOracle();
+        uint borrow_amount = EthPrice*contributeAmount;
+        Ceth_interface.mint{value: contributeAmount, gas: 300000 }();
+        enterMarket();
+        borrowDai(borrow_amount);
+        daiToEth(borrow_amount);
         return true;
     }
+
+    function EthpriceOracle() public view returns 
+        (
+        uint)
+    {
+        return oracle_interface.getUnderlyingPrice(Ceth_interface)/(10**18);
+    }
+
+    // function supplyEthToCompound()
+    //     public
+    //     payable
+    //     returns (bool) {
+    //     // Mint CEth
+    //     Ceth_interface.mint{value: msg.value, gas: 300000 }();
+    //     return true;
+    // }
     
-    //1000000000000000 wei or 0.001 eth or -> 0.1 usd
+    //1000000000000000 wei or 0.01 eth or -> 0.1 usd
     //0x065eE5Dfc2Ced07dD49DaCB7849cE2F84EaABA0E wallet address
 
-    function redeemCEth() 
+    // function redeemCEth() 
+    //     public 
+    //     returns (bool) 
+    //     {
+    //     uint Ctoken_amount = Ceth_interface.balanceOf(address(this));
+    //     require(Ceth_interface.redeem(Ctoken_amount) == 0, "Something went wrong");
+    //     return true;
+    // }
+
+    function redeemCEth(uint _amount) 
         public 
         returns (bool) 
         {
-        uint Ctoken_amount = Ceth_interface.balanceOf(address(this));
-        require(Ceth_interface.redeem(Ctoken_amount) == 0, "Something went wrong");
+        // uint Ctoken_amount = Ceth_interface.balanceOf(address(this));
+        require(Ceth_interface.redeem(_amount) == 0, "Something went wrong");
         return true;
     }
 
-    // dai 0xdc31ee1784292379fbb2964b3b9c4124d8f89c60
-    // cdai 0x822397d9a55d0fefd20f5c4bcab33c5f65bd28eb
-
-    // borrow 100000000000000000 or 0.1 Dai
+    // borrow 1000000000000000000 or 1 Dai
     function borrowDai(
         uint _borrowAmount) 
-        public payable
-        {
+        public 
+        payable {
         require(cDai_interface.borrow(_borrowAmount) == 0, "got collateral?");
     }
 
-    function getAccountLiquidity() public view returns (uint, uint, uint){
+    function getAccountLiquidity() 
+        public
+        view 
+        returns (uint, uint, uint)
+        {
         // Comptroller comp = Comptroller(comptroller_address);
         (uint error, uint liquidity, uint shortfall) = comp_interface.getAccountLiquidity(address(this));
         return (error, liquidity, shortfall);
     }
 
-    function currentEth() public view returns(uint){
+    function currentEth() 
+        public 
+        view 
+        returns(uint)
+        {
         uint amount_temp = (Ceth_interface.exchangeRateStored() * Ceth_interface.balanceOf(address(this)))/10**18;
         return amount_temp;
     }
 
-    function sendViaCall(address payable _to) public payable {
-        // Call returns a boolean value indicating success or failure.
-        // This is the current recommended method to use.
+    function sendViaCall(
+        address payable _to
+        ) 
+        public 
+        payable {
         (bool sent, bytes memory data) = _to.call{value: address(this).balance}("");
         require(sent, "Failed to send Ether");
     }
 
-    function send_money_back() public {
+    function sendMoneyBack() 
+        public 
+        {
         address payable to_sender = payable(msg.sender);
         sendViaCall(to_sender);
     }
 
-    function enter_market() public {
+    function enterMarket() 
+        public 
+        {
         address[] memory markets = new address[](1);
         markets[0] = cEth_adress;
         comp_interface.enterMarkets(markets);
     }
 
-    function repay_dai() public {
-        Dai_interface.approve(cDai_address, Dai_interface.balanceOf(address(this)));
-        cDai_interface.repayBorrow((2**256)-1);
-    }
+    // function repay_dai() public {
+    //     Dai_interface.approve(cDai_address, Dai_interface.balanceOf(address(this)));
+    //     cDai_interface.repayBorrow((2**256)-1);
+    // }
 
-    function repay_dai2(uint _amount) public {
+    function repayDai(uint _amount) 
+        public 
+        {
         Dai_interface.approve(cDai_address, Dai_interface.balanceOf(address(this)));
         cDai_interface.repayBorrow(_amount);
+    }
+
+    function daiToEth(uint _amount) 
+        public 
+        {
+        require(Dai_interface.approve(address(uniswap_rounter_address), Dai_interface.balanceOf(address(this))), "approve failed.");
+        address[] memory path = new address[](2);
+        path[0] = address(dai_address);
+        path[1] = uniswap_interface.WETH();
+        uniswap_interface.swapExactTokensForETH(_amount, 0, path, address(this), block.timestamp);
+    }
+
+    function ethToDai(uint _amount) public {
+        require(Dai_interface.approve(address(uniswap_rounter_address), Dai_interface.balanceOf(address(this))), "approve failed.");
+        address[] memory path = new address[](2);
+        path[0] = uniswap_interface.WETH();
+        path[1] = address(dai_address);
+        uniswap_interface.swapExactETHForTokens{value: _amount }(0, path, address(this), block.timestamp);
     }
 
     // This is needed to receive ETH when calling `redeemCEth`
